a4d0cee8d6a9c6952227280602f6523067a49d6b
Manikanta N <manikanta.thayappa@gmail.com>
2025-02-18 14:42:09 -0600

    popup for search and fetching vulerability details and displaying is completed including search and pagination

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { VulnerabilityPopupComponent } from './components/vulnerability-popup/vulnerability-popup.component';
+import { DisplayVulnerabilitiesComponent } from './components/display-vulnerabilities/display-vulnerabilities.component';
 
 export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
@@ -11,5 +13,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent},
-  { path: 'reset-password', component: ResetPasswordComponent}
+  { path: 'reset-password', component: ResetPasswordComponent},
+  { path: 'vulnerability', component: VulnerabilityPopupComponent },
+  { path: 'display-vulnerabilities',component: DisplayVulnerabilitiesComponent}
 ];
