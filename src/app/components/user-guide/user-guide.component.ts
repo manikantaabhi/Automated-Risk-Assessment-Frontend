5d748c7156128555959fdf5f90bf42dadb0bbc35
Janani Sree <jananisree@Jananis-MacBook-Air.local>
2025-04-17 11:33:55 -0500

    added styles

--- src/app/components/user-guide/user-guide.component.ts
+++ src/app/components/user-guide/user-guide.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-user-guide',
-  imports: [CommonModule],
+  imports: [CommonModule,HeaderComponent],
   templateUrl: './user-guide.component.html',
   styleUrl: './user-guide.component.css'
 })
 export class UserGuideComponent {
+  selectedService: string = ''; // Tracks the highlighted service
 
   expandedSections: boolean[] = [false, false, false, false, false];
 
   toggleSection(index: number): void {
     this.expandedSections[index] = !this.expandedSections[index];
+  }
+  highlightService(serviceId: string) {
+    this.selectedService = serviceId;
+    setTimeout(() => {
+      this.selectedService = ''; // Remove highlight after 3 seconds
+    }, 3000);
   }
 
 }
